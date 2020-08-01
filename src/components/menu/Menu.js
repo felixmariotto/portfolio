b8a76a54bf28de90bd0b372aa57700109c909353
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-08-01 21:46:49 +0200

    slide to show more content when click in menu button

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,6 +6,61 @@ import './menu.css';
 const container = document.createElement('DIV');
 container.id = "menu";
 
+const content = document.createElement('DIV');
+content.id = "menu-content";
+
+container.append( content );
+
+//
+
+const linksContainer = document.createElement('DIV');
+linksContainer.id = "links-container";
+
+content.append( linksContainer );
+
+//
+
+const moduleContainer = document.createElement('DIV');
+moduleContainer.id = "module-container";
+moduleContainer.innerHTML = 'module';
+
+content.append( moduleContainer );
+
+const arrowBack = document.createElement('I');
+arrowBack.classList.add('fa', 'fa-long-arrow-left');
+arrowBack.id = "module-container-arrow-back";
+
+moduleContainer.append( arrowBack );
+
+//
+
+linksContainer.append( MenuLink( 'expertise', 'fff' ) );
+linksContainer.append( MenuLink( 'prototypes', 'aa' ) );
+linksContainer.append( MenuLink( 'casting parts', 'gr' ) );
+linksContainer.append( MenuLink( 'technical doc', 'nn' ) );
+
+linksContainer.append( MenuLink( 'contact', 'gre' ) );
+
+//
+
+function MenuLink( text, componentToOpen ) {
+
+	const link = document.createElement('DIV');
+	link.classList.add( 'menu-link' );
+	link.innerHTML = text;
+
+	link.addEventListener( 'click', () => {
+
+		content.classList.add( 'expanded' );
+
+		console.log( componentToOpen )
+
+	});
+
+	return link
+
+}
+
 //
 
 export default container
