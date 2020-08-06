7d55c10463592c830283bab77415d6c5fa3130d1
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-08-06 13:14:57 +0200

    make particles stir lightly from right to left

--- src/components/homepage/scenes/core/Particles.js
+++ src/components/homepage/scenes/core/Particles.js
@@ -16,13 +16,16 @@ export default function Particles() {
 
 		const vec = new THREE.Vector3();
 
+		vec.initX = (Math.random() - 0.5) * 1.4;
+
 		vec.set(
-			(Math.random() - 0.5) * 1.4,
+			vec.initX,
 			(Math.random() - 0.5) * 0.8,
 			0.3 + ( Math.random() * 0.5 )
 		);
 
 		vec.upSpeed = 0.0003 + ( Math.random() * 0.0005 );
+		vec.t = Math.random() * Math.PI;
 
 		spritePositions.push( vec );
 
@@ -46,9 +49,13 @@ export default function Particles() {
 
 	function update() {
 
-		spritePositions.forEach( (spritePos) => {
+		spritePositions.forEach( (spritePos, i) => {
 
 			spritePos.y += spritePos.upSpeed;
+
+			spritePos.t += 0.004;
+
+			spritePos.x = spritePos.initX + ( Math.sin(spritePos.t) * 0.05 );
 
 			if ( spritePos.y > 0.4 ) spritePos.y = -0.4;
 
