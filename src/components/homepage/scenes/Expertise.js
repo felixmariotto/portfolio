a4508a63f5a52b913cd140b8646765c1c30ceb99
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-08-18 10:37:16 +0200

    expertise scene : increase camera move

--- src/components/homepage/scenes/Expertise.js
+++ src/components/homepage/scenes/Expertise.js
@@ -21,42 +21,6 @@ export default function Expertise( domElement ) {
 
 	});
 
-	/*
-	// table
-
-	const tableGeometry = new THREE.BoxBufferGeometry( 1.5, 0.02, 1.5 );
-	const tableMaterial = new THREE.MeshLambertMaterial({ color: 0xababab });
-
-	scene.add( new THREE.Mesh( tableGeometry, tableMaterial ) );
-
-	// papers
-
-	const paperGeometry = new THREE.PlaneBufferGeometry( 0.21, 0.297 );
-	paperGeometry.rotateX( -Math.PI / 2 );
-	const paperMaterial = new THREE.MeshBasicMaterial({ color: 0xebe9e4 });
-
-	const paper1 = new THREE.Mesh( paperGeometry, paperMaterial );
-	paper1.position.set( -0.1, 0.2, 0 );
-	paper1.rotation.y = 0.1;
-	scene.add( paper1 );
-
-	const paper2 = new THREE.Mesh( paperGeometry, paperMaterial );
-	paper2.position.set( 0.1, 0.2, 0.1 );
-	paper2.rotation.y = -0.2;
-	scene.add( paper2 );
-
-	// pen
-
-	const penGeometry = new THREE.BoxBufferGeometry( 0.013, 0.013, 0.15 );
-	const penMaterial = new THREE.MeshLambertMaterial({ color: 0xb06bff });
-
-	const pen = new THREE.Mesh( penGeometry, penMaterial );
-	pen.position.set( -0.05, 0.2, 0 );
-	pen.rotation.y = -0.5;
-
-	scene.add( pen );
-	*/
-
 	// light
 
 	var light = new THREE.PointLight( 0xb6ffb3, 0.8, 100 );
@@ -77,14 +41,23 @@ export default function Expertise( domElement ) {
 	//
 
 	const targetRot = new THREE.Vector2();
+	let targetPos = 0;
 
 	function animate() {
 
-		targetRot.y = 0.08 * -InputPosition.x;
-		targetRot.x = 0.04 * -InputPosition.y;
+		targetRot.y = 0.2 * -InputPosition.x;
+		targetRot.x = 0.2 * -InputPosition.y;
 
 		cameraGroup.rotation.x += ( targetRot.x - cameraGroup.rotation.x ) * 0.02;
 		cameraGroup.rotation.y += ( targetRot.y - cameraGroup.rotation.y ) * 0.02;
+
+		//
+
+		targetPos = InputPosition.x * -0.04;
+
+		cameraGroup.position.x += ( targetPos - cameraGroup.position.x ) * 0.05;
+
+		//
 
 		renderer.render( scene, camera );
 
