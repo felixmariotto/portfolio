d1f4f9145caacd06554c2c058eca934478df8c41
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-14 14:53:16 +0200

    serve favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon')
 
 //
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000
 //
 
 const app = express()
+
+	// .use( favicon( path.join( __dirname, 'src', 'data', 'favicon.ico') ) )
 
 	.use( express.static( path.join( __dirname, 'dist' ) ) )
 
