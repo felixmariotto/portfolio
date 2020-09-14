5cc2c691d577daa456893831f9cc290482e988f1
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-14 11:05:35 +0200

    add animated download icon to samples info module

--- src/components/modules/Samples.js
+++ src/components/modules/Samples.js
@@ -22,27 +22,59 @@ container.append( samplesContainer );
 
 //
 
+const updateFns = [];
+
 createSamplesBox( 'Photorealistic Rendering' );
 
 createSamplesBox( 'CAD 3D Models' );
 
 createSamplesBox( 'Documentation' );
 
+container.onscroll = () => updateFns.forEach( fn => fn() );
+
 //
 
 function createSamplesBox( title ) {
 
-	const container = document.createElement('DIV');
-	container.classList.add('sample-container');
+	const sampleContainer = document.createElement('DIV');
+	sampleContainer.classList.add('sample-container');
 
 	const img = document.createElement('IMG');
 	img.src = 'https://cad-portfolio.s3.eu-west-3.amazonaws.com/textures/viewer2.jpg';
 
+	const icon = document.createElement('I');
+	icon.classList.add('fa', 'fa-download');
+
 	const text = document.createElement('DIV');
 	text.innerHTML = title;
 
-	container.append( img, text );
-	samplesContainer.append( container );
+	sampleContainer.append( icon, img, text );
+	samplesContainer.append( sampleContainer );
+
+	updateIconVisibility();
+
+	function updateIconVisibility() {
+
+		const contRect = container.getBoundingClientRect();
+
+		const rect = sampleContainer.getBoundingClientRect();
+
+		if ( 
+			contRect.bottom < rect.top + ( ( rect.bottom - rect.top ) * 0.7 ) ||
+			contRect.top > rect.bottom - ( ( rect.bottom - rect.top ) * 0.7 )
+		) {
+
+			icon.classList.add('hidden')
+
+		} else {
+
+			icon.classList.remove('hidden')
+
+		};
+
+	};
+
+	updateFns.push( updateIconVisibility );
 
 };
 
