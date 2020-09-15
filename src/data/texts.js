50831f08db0bbc65d6b0d8850c9840ec87ebccf0
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-15 15:03:52 +0200

    translate texts in French

--- src/data/texts.js
+++ src/data/texts.js
@@ -1,6 +1,10 @@
 
 const texts = {
 
+	////////////
+	// ENGLISH
+	////////////
+
 	english: {
 
 		// HOMEPAGE
@@ -23,9 +27,10 @@ const texts = {
 
 			expertise: {
 				title: "Consulting",
+
 				text: `Backed by my experience working with the most demanding 
-						high jewelry companies, I can advise you 
-						wisely with your projects.`
+						high jewelry companies, I am able to advise you 
+						in your projects.`
 			},
 
 			// PROTOTYPES SCENE
@@ -56,8 +61,7 @@ const texts = {
 			doc: {
 				title: "Documentation",
 				text: `I can provide the documentation that suit your need.  
-						Assembly instructions, stone setting blueprints, painting layouts, 
-						everything is covered.`
+						Assembly instructions, stone setting blueprints, painting layouts...`
 			},
 
 			// CONTACT SCENE
@@ -65,7 +69,7 @@ const texts = {
 			contact: {
 				title: "More information :",
 				webdevLink: 'Web development',
-				samplesLink: 'Review samples',
+				samplesLink: 'Samples',
 				contactLink: 'Contact'
 			}
 
@@ -94,7 +98,7 @@ const texts = {
 				
 					speech2: `Armed with the design and project management 
 								skills acquired during these years, 
-								I can give you precious advices to make 
+								I can give you precious advices to helper you making 
 								your projects happen.`
 				},
 
@@ -113,7 +117,8 @@ const texts = {
 			prototypes: {
 				title: 'Prototypes & 3D viewers',
 
-				speech1: `Designing is never a seamless process. 
+				speech1: `Designing is a progressive process, 
+							scattered with unexpected issues. 
 							Our task will be to work jointly to 
 							reveal all the unforeseen details and 
 							oversights of the original project, 
@@ -156,17 +161,18 @@ const texts = {
 			// DOCUMENTATION
 
 			doc: {
-				title: 'Technical Documentation',
+				title: 'Documentation',
 
 				speech1: `Once your project is designed, 
-							you are not left alone in the wild. 
+							you are not left on your own. 
 							I send you all the documentation you need to 
 							assemble the parts, set the stones, 
 							and even communicate and advertise about it.`,
 
 				speech2: `I adapt the documentation to every project, 
-							and listen to your special demands, 
-							be it parts spreadsheets for your inventory, documents for public presentations 
+							and listen to your special demands : 
+							parts spreadsheets for your inventory, 
+							documents for public presentations 
 							or customer use, 4-views layouts for painting... 
 							Name your own needs.`
 			},
@@ -179,7 +185,7 @@ const texts = {
 				speech1: `Web development has been my passion 
 							for a couple of years, and I offer my 
 							services in this field. My skills in jewelry, 
-							3D modeling and web development is a rare 
+							3D modeling and web development are a rare 
 							and precious combination if you are a 
 							jewelry company with a project of high-end online service.`,
 
@@ -204,21 +210,231 @@ const texts = {
 
 		}
 
+	},
+
+	///////////
+	// FRENCH
+	///////////
+
+	french: {
+
+		// HOMEPAGE
+
+		homepage: {
+
+			// GLOBAL
+
+			global: {
+				more: "voir plus"
+			},
+
+			// INTRO SCENE
+
+			intro: {
+				intro: 'Design Haute Joaillerie'
+			},
+
+			// EXPERTISE SCENE
+
+			expertise: {
+				title: "Conseil",
+
+				text: `Fort d'une expérience de travail avec les maisons de 
+						joaillerie les plus exigeantes, je suis à même de vous 
+						conseiller dans vos projects.`
+			},
+
+			// PROTOTYPES SCENE
+
+			prototypes: {
+				title: "Prototypes & rendus",
+
+				text: `Tôt pendant le développement,
+						vous examinerez des prototypes en résine
+						et des rendus photoréalistes, pour 
+						identifier les problèmes rapidement et
+						respecter vos échéances.`
+			},
+
+			// CASTING PARTS SCENE
+
+			casting: {
+				title: "Pièces de fonte",
+				text: `Une fois votre bijou conçu,
+						Je peux fournir des pièces de fonte
+						à cire perdu en métaux variés : 
+						or, argent, platine, et même titane.`
+			},
+
+			// DOCUMENTATION SCENE
+
+			doc: {
+				title: "Documentation",
+				text: `Je peux fournir la documentation
+						dont vous avez besoin. Plans de montage,
+						nomenclatures, dessins pour gouache...`
+			},
+
+			// CONTACT SCENE
+
+			contact: {
+				title: "Plus d'information :",
+				webdevLink: 'Développement web',
+				samplesLink: 'Échantillons',
+				contactLink: 'Contact'
+			}
+
+		},
+
+		// INFO MODULES
+
+		modules: {
+
+			// EXPERTISE
+
+			expertise: {
+				title: "Conseil",
+
+				presentation: {
+					title: "Qui suis-je ?",
+
+					speech1: `J'ai travaillé pendant plus de deux ans chez
+								<i>Lasbleiz Fournier Vitiello</i> en tant
+								que CAO ( Concepteur Assisté par Ordinateur ),
+								cet atelier est connu dans la communeauté de
+								la place Vendôme pour son travail de grande qualité.
+								J'ai ensuite vécu trois ans à Hong Kong où
+								j'ai travaillé pour <i>Michelle Ong</i>,
+								qui fabrique des bijoux pour les plus fins 
+								connaisseurs de cette cité-état.`,
+				
+					speech2: `Armé de ces compétences en design et en gestion,
+								je peux vous offrir un conseil précieux pour
+								vous aider à réaliser vos projets.`
+				},
+
+				service: {
+					title: "Mon service",
+
+					speech: `Vous pouvez m'engager pour gérer vos projets 
+								de design, ou en tant que consultant pour 
+								vous aider à prendre la bonne décision 
+								quand vous avez un doute.`
+				}
+				
+			},
+
+			// PROTOTYPES
+
+			prototypes: {
+				title: 'Prototypes & rendus',
+
+				speech1: `Le design est un processus évolutif,
+							parsemé d'imprévus. Notre tâche sera de 
+							travailler conjointement pour révéler les
+							problèmes conceptuels du design originel,
+							pour pouvoir arriver à un produit esthétiquement
+							et techniquement parfait à la date de
+							livraison que vous aurez défini.`,
+
+				speech2: `Pour obtenir ce design parfait et nous 
+							épargner la découverte de défaults tard 
+							dans la conception du produit, nous 
+							organiseront régulièrement des révisions
+							du design, supportées par des prototypes
+							en résine et/ou des rendus 3D photoréalistes.`,
+
+				speech3: `Si vous ne possédez pas d'imprimante 3D pour
+							l'impression des prototypes, pas d'inquiétude : 
+							je m'en occupe pour vous, et vous les fait
+							livrer à votre bureau.`
+			},
+
+			// CASTING
+
+			casting: {
+				title: 'Pièces de fonte',
+
+				speech1: `Votre projet est conçu et dessiné. Vous pouvez
+							maintenant choisir la fonte à cire perdue via
+							l'impression 3D pour accélerer la réalisation, 
+							et vous assurer que le bijou fini sera au plus 
+							proche de ce que nous avons conçu ensemble. 
+							Armé de connaissances pratiques en fabrication
+							de bijou, particulièrement dans le domaine
+							du sertissage, je peux adapter votre design
+							aux contraintes technique de l'impression 3D,
+							de la fonte à cire perdue, et de toutes les
+							étapes ultérieures de fabrication.`,
+
+				speech2: `Si vous ne possédez pas votre propre imprimante 3D
+							adaptée à la fonte à cire perdue, je me propose de 
+							vous envoyer les fontes directement à votre atelier
+							par transporteur sécurisé.`
+			},
+
+			// DOCUMENTATION
+
+			doc: {
+				title: 'Documentation',
+
+				speech1: `Une fois votre projet conçu, vous n'êtes pas
+							laissé à vous-même. Je vous envoie toute la 
+							documentation dont vous avez besoin pour 
+							assembler les éléments, sertir les pierres, 
+							et même communiquer sur votre bijou.`,
+
+				speech2: `J'adapte la documentation à chaque projet,
+							et répond à vos demandes spéciale :
+							tableaux pour vos inventaires, documents pour
+							vos présentations publiques ou privées, plans 
+							quatre vues à gouacher... Vous n'avez qu'à
+							demander.`
+			},
+
+			// WEB DEVELOPMENT
+
+			webdev: {
+				title: 'Développement web',
+
+				speech1: `Le développement web a été ma passion 
+							ces dernières années, et j'offre mes services
+							dans ce domaine. Mes compétences en joaillerie,
+							modélisation 3D et web développement sont une
+							combinaison rare et précieuse si vous êtes une
+							entreprise de joaillerie avec un projet de service
+							en ligne.`,
+
+				speech2: `Ce site que vous visitez fut entièrement designé et
+							programmé par moi-même avec les dernières
+							technologies web (&#xA0;Node.js, Three.js, 
+							Webpack, etc...&#xA0;). Les modèles 3D affichés
+							dans chaque scène de la page d'accueil sont les miens,
+							et on nécessité un traitement spécial pour le
+							rendu en temps réel.`
+			},
+
+			// CONTACT
+
+			contact: {
+				title: 'Contact',
+
+				speech: `Je serai ravi de répondre à toute demande
+							d'information, et de faire un devis ajusté
+							à vos besoins. Si vous voulez discuter de
+							vive voix, nous pouvons organiser une entrevue
+							via Skype ou Zoom.`
+			}
+
+		}
+
 	}
 
 }
 
-////////////
-// FRENCH
-////////////
+//
 
-
-
-////////////
-// ENGLISH
-////////////
-
-const currentLanguage = 'english';
+const currentLanguage = 'french';
 
 function getLanguageSet() {
 
